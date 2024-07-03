fc725123b117c2a0f1a127ebfe312d7c7789be0d
Ivan Abramovich <ivan.abramovich@eltex.loc>
2024-07-03 12:24:37 +0700

    shortcut for save

--- init.lua
+++ init.lua
@@ -162,6 +162,7 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<C-s>', '<cmd>:w<cr>')
 
 --Custom keymap
 vim.keymap.set('i', 'jk', '<Esc>')
