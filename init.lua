590d10717b469d44d94a95cc04deea17b1893e9e
Ivan Abramovich <ivan.abramovich@eltex.loc>
2024-10-22 09:45:39 +0700

    added more harpoon maps

--- init.lua
+++ init.lua
@@ -894,6 +894,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>ha', function() mark.add_file() end, { desc = '[H]arpoon  [A]dd' })
       vim.keymap.set('n', '<leader>hn', function() ui.nav_next() end, { desc = '[H]arpoon [N]ext' })
       vim.keymap.set('n', '<leader>hp', function() mark.add_file() end, { desc = '[H]arpoon [P]revious' })
+      vim.keymap.set('n', '<leader>ht', "<CMD>Telescope harpoon marks<CR>", { desc = '[H]arpoon [T]elescope' })
+      vim.keymap.set('n', '<leader>h1', function() ui.nav_file(1) end, { desc = '[H]arpoon [1] mark' })
+      vim.keymap.set('n', '<leader>h2', function() ui.nav_file(2) end, { desc = '[H]arpoon [2] mark' })
+      vim.keymap.set('n', '<leader>h3', function() ui.nav_file(3) end, { desc = '[H]arpoon [3] mark' })
+      vim.keymap.set('n', '<leader>h4', function() ui.nav_file(4) end, { desc = '[H]arpoon [4] mark' })
 
       require('harpoon').setup({
         mark_branch = false
