48938df2fbb4b7acd4e9e0a9dfa076c45f4ef1f4
Ivan Abramovich <ivan.abramovich@eltex.loc>
2025-01-30 13:11:59 +0700

    added another binding for telescope resume

--- init.lua
+++ init.lua
@@ -471,7 +471,7 @@ require('lazy').setup({
       --   { desc = '[S]earch by grep in [O]pen buffers' })
       -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       -- vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
-      -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sR', builtin.resume, { desc = '[S]earch [R]esume' })
       -- vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sl', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
