9799d3474e76e4c97ead7355b9f5ec47ff1c7748
Ivan Abramovich <ivan.abramovich@eltex.loc>
2025-04-04 17:39:59 +0700

    catpuccin theme

--- init.lua
+++ init.lua
@@ -662,7 +662,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'navarasu/onedark.nvim',
+    "catppuccin/nvim",
     --'catppuccin/nvim',
     --'ellisonleao/gruvbox.nvim',
     --"rose-pine/neovim",
@@ -673,7 +673,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
