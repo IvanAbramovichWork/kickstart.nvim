43ef7e852bd79b247470844ab295e7c4f8065d3d
Ivan Abramovich <ivan.abramovich@eltex.loc>
2025-06-17 17:23:21 +0700

    replaced erlanglg with elp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -155,7 +155,8 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      erlangls = {},
+      -- erlangls = {},
+      elp = {},
       elixirls = {},
       lemminx = {},
       -- clangd = {},
