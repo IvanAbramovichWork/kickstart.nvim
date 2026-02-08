089dd353cfa85ee31a4284b7cdef8789d3626a89
Ivan Abramovich <ivan.abramovich@eltex.loc>
2026-02-08 13:09:33 +0700

    added snacks explorer

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -22,6 +22,7 @@ return {
   keys = {
     { "<leader>gg",       function() Snacks.lazygit() end,                                                desc = "Lazygit" },
     { "gr",               function() Snacks.picker.lsp_references() end,                                  nowait = true,                     desc = "Lsp references" },
+        { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
     { "gai",              function() Snacks.picker.lsp_incoming_calls() end,                              desc = "C[a]lls Incoming" },
     { "gao",              function() Snacks.picker.lsp_outgoing_calls() end,                              desc = "C[a]lls Outgoing" },
     { "<leader>sf",       function() Snacks.picker.files({ ignored = true, exclude = { '*.beam' } }) end, desc = "Files" },
